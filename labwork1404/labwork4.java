b0a913dfb3a9dbc772bb3682face01bef6be9057
Afsha Ansari <59908603+Afsha138@users.noreply.github.com>
2022-04-21 13:43:03 +0530

    Add files via upload

---
+++ labwork1404/labwork4.java
@@ -0,0 +1,25 @@
+public class Labwork4
+
+{
+
+  public static void main(String args[])
+
+  {
+
+    float averageSalary= 10000;
+    
+    float employeeA=11000;
+
+    float employeeB=5000;
+  
+    float employeeC;
+  
+    employeeC=(averageSalary*3)-
+(employeeA+employeeB);
+ 
+   System.out.println("Third employee earn "
++employeeC);
+  
+   }
+
+}
